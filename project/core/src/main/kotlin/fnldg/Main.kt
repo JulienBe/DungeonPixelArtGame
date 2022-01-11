298f5db0b62420082aec04839aeca93a439b0e68
JulienBe <bertozzijulien@gmail.com>
2022-01-11 21:34:33 +0100

    load image to pixmap

--- project/core/src/main/kotlin/fnldg/Main.kt
+++ project/core/src/main/kotlin/fnldg/Main.kt
@@ -1,5 +1,6 @@
 package fnldg
 
+import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
@@ -11,25 +12,34 @@ import ktx.assets.toInternalFile
 import ktx.graphics.use
 
 class Main : KtxGame<KtxScreen>() {
-    override fun create() {
-        addScreen(FirstScreen())
-        setScreen<FirstScreen>()
-    }
+  override fun create() {
+    addScreen(FirstScreen())
+    setScreen<FirstScreen>()
+  }
 }
 
 class FirstScreen : KtxScreen {
-    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
-    private val batch = SpriteBatch()
+  private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
+  private val batch = SpriteBatch()
+  private val pixmap = getPixMap(image)
 
-    override fun render(delta: Float) {
-        clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
-        batch.use {
-            it.draw(image, 100f, 160f)
-        }
-    }
+  fun getPixMap(t: Texture): Pixmap {
+    val td = t.textureData
+    td.prepare()
+    return td.consumePixmap()
+  }
 
-    override fun dispose() {
-        image.disposeSafely()
-        batch.disposeSafely()
+  override fun render(delta: Float) {
+    clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
+
+
+    batch.use {
+      it.draw(image, 100f, 160f)
     }
+  }
+
+  override fun dispose() {
+    image.disposeSafely()
+    batch.disposeSafely()
+  }
 }
--- project/lwjgl3/src/main/kotlin/fnldg/lwjgl3/Lwjgl3Launcher.kt
+++ project/lwjgl3/src/main/kotlin/fnldg/lwjgl3/Lwjgl3Launcher.kt
@@ -8,9 +8,9 @@ import fnldg.Main
 
 /** Launches the desktop (LWJGL3) application. */
 fun main() {
-    Lwjgl3Application(Main(), Lwjgl3ApplicationConfiguration().apply {
-        setTitle("FinalDungeon")
-        setWindowedMode(640, 480)
-        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
-    })
+  Lwjgl3Application(Main(), Lwjgl3ApplicationConfiguration().apply {
+    setTitle("FinalDungeon")
+    setWindowedMode(640, 480)
+    setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
+  })
 }
